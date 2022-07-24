45bee09ef6bf1fd2faa145cc207b984078fb3414
Your Name <you@example.com>
2022-07-24 20:31:44 +0400

    Report Validations added to the system.

--- UserManagementInTerminal/UserManagementInTerminal/ApplicationLogic/Validation/ReportValidation.cs
+++ UserManagementInTerminal/UserManagementInTerminal/ApplicationLogic/Validation/ReportValidation.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AuthenticationWithClie.ApplicationLogic.Validations;
 
-namespace UserManagementInTerminal.ApplicationLogic.Validation
+namespace UserManagement.ApplicationLogic.Validation
 {
-    internal class ReportValidation
+    public class ReportValidation
     {
+        public static bool IsReportValid(string firstName, int start, int end)
+        {
+            if (Validations.IsLengthBetween(firstName, start, end))
+            {
+                return true;
+            }
+
+            Console.WriteLine("Report's length should be more than 3 and less than 30!");
+            return false;
+        }
+
+        public static string GetReport()
+        {
+            bool IsExceptionValid;
+            string text = null;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Please enter report : ");
+                    text = Console.ReadLine();
+
+                    if (text == "null")
+                    {
+                        throw new Exception();
+                    }
+
+                    IsExceptionValid = false;
+                }
+
+                catch (Exception)
+                {
+                    IsExceptionValid = true;
+                    Console.WriteLine("There is an error");
+                }
+            }
+
+            while (IsExceptionValid || !ReportValidation.IsReportValid(text, 3, 30));
+
+            return text;
+        }
     }
 }
