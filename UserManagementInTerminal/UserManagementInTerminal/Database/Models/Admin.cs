1b2d8053d7db871b5e8a238be088ade99c800a66
Your Name <you@example.com>
2022-07-24 20:25:01 +0400

    Get Info added.

--- UserManagementInTerminal/UserManagementInTerminal/Database/Models/Admin.cs
+++ UserManagementInTerminal/UserManagementInTerminal/Database/Models/Admin.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using AuthenticationWithClie.Database.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserManagementInTerminal.Database.Models
+namespace AuthenticationWithClie.Database.Models
 {
-    internal class Admin
+    public class Admin : User
     {
+        public static int row = 1;
+        public int Row { get; set; }
+
+        public Admin(string firstName, string lastName, string email, string password, int id)
+            : base(firstName, lastName, email, password, id)
+        {
+            Row = row++;
+        }
+
+        public Admin(string firstName, string lastName, string email, string password)
+            : base(firstName, lastName, email, password)
+        {
+            Row = row++;
+        }
+
+        public Admin(string firstName, string lastName)
+            : base(firstName, lastName)
+        {
+
+        }
+
+        public override string GetInfo()
+        {
+            return $"Row No : {Row}, User Id : {Id}, First name : {FirstName}, Last name :  {LastName}, Email :  {Email}, Registration Date : {RegistrationDate}";
+        }
     }
 }
