ced5c93cb8e5556494b48b75e1bdce5f7aedc11b
Your Name <you@example.com>
2022-07-24 20:23:12 +0400

    Get user report info and get admin report info added.

--- UserManagementInTerminal/UserManagementInTerminal/Database/Models/Report.cs
+++ UserManagementInTerminal/UserManagementInTerminal/Database/Models/Report.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UserManagementInTerminal.Database.Models
+namespace UserManagement.Database.Models
 {
-    internal class Report
+    public class Report
     {
+        public static int row = 1;
+        public int Row { get; set; }
+        public string Reporter { get; set; }
+        public string Receiver { get; set; }
+        public string Text { get; set; }
+        public bool IsReceiverAdmin { get; set; }
+        public DateTime ReportTime { get; set; }
+
+        public Report(string reporter, string receiver, string text, bool isReceiverAdmin = false)
+        {
+            Row = row++;
+            Reporter = reporter;
+            Receiver = receiver;
+            Text = text;
+            IsReceiverAdmin = isReceiverAdmin;
+            ReportTime = DateTime.Now;
+        }
+
+        public virtual string GetUserReportInfo()
+        {
+            return $"Row No : {Row}, Reporter : {Reporter}, Receiver : {Receiver}, Text : {Text}, Is the person receiving the text admin : {IsReceiverAdmin}, Report time : {ReportTime}";
+        }
+
+        public string GetAdminReportInfo()
+        {
+            return $"Row No : {Row}, Reporter : {Reporter}, Receiver : {Receiver}, Text : {Text}, Is the person receiving the text admin : {IsReceiverAdmin = true}, Report time : {ReportTime}";
+        }
     }
 }
