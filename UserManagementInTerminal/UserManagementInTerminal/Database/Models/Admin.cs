cffb56dc0c49cdee5a3696516d1bd4dc5ce76ee4
Your Name <you@example.com>
2022-07-24 20:15:16 +0400

    Login and Register Added

---
+++ UserManagementInTerminal/UserManagementInTerminal/Database/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagementInTerminal.Database.Models
+{
+    internal class Admin
+    {
+    }
+}
---
+++ UserManagementInTerminal/UserManagementInTerminal/UI/Program.cs
@@ -0,0 +1,46 @@
+﻿using AuthenticationWithClie.ApplicationLogic;
+using System;
+
+namespace AuthenticationWithClie.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our website!");
+            Console.WriteLine();
+
+            Console.WriteLine("PAvailable Commands :");
+            Console.WriteLine("/register");
+            Console.WriteLine("/login");
+            Console.WriteLine("/exit");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Please enter command : ");
+                string command = Console.ReadLine();
+
+                if (command == "/register")
+                {
+                    Authentication.Register();
+                }
+
+                else if (command == "/login")
+                {
+                    Authentication.Login();
+                }
+
+                else if (command == "/exit")
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Command not found!");
+                }
+            }
+        }
+    }
+}
